fe0979582141ba271e0163c197514a8efdc7d121
avanijayantkale <Avanikale20>
2019-09-01 20:33:55 +0530

    [Avani] Add. Friends Class and Test class

---
+++ src/main/java/com/thoughtworks/bootcamp/Friends.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.bootcamp;
+
+public class Friends {
+
+    private String name;
+    private  double amountToGive;
+    private double amountGiven;
+
+    public Friends(String name, double amountToGive, double amountGiven)
+    {
+        this.name=name;
+        this.amountToGive=amountToGive;
+        this.amountGiven=amountGiven;
+    }
+
+
+    public double calculateExpensesForEachFriend() {
+        return 0.0;
+    }
+}
